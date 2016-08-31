0a62251a4481922656221d9a9831e7e9e128f0f8
test <test@abc.com>
2016-08-31 15:09:08 +0700

    read json finish

--- app/src/main/java/rd/angkanasopit/wallop/rdrun/MainActivity.java
+++ app/src/main/java/rd/angkanasopit/wallop/rdrun/MainActivity.java
@@ -1,13 +1,19 @@
 package rd.angkanasopit.wallop.rdrun;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ViewPropertyAnimatorCompatSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     //Expicit
     private ImageView imageView;
     private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,67 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
 
+    //create Iner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+        //Explicit
+        private Context context;
+        private String myUserString, myPasswordString;
+        private static final String urlJSON = "http://swiftcodingthai.com/rd/get_user_master.php";
+
+        public SynUser(Context context, String myUserString, String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("31AugV2", "e doInBack ==>" + e.toString());
+                return null;
+            }
+
+
+        }   //doInBak
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("31AugV2", "JSon ==> " + s);
+
+
+        }   //onPost
+
+    }   //SynUser Class
+
+
+
+
 
     public void clickSignInMain(View view) {
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check space
+        if (userString.equals("") || passwordString.equals("")) {
+            //have space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDiaog(this, R.drawable.rat48, "Have Space", "Pleas Fill All Every Blank");
+
+        } else {
+            //no space
+            SynUser synUser = new SynUser(this, userString,passwordString);
+            synUser.execute();
+
+        }
 
     }   //ciickSignIn
 
