a8f7cd806653cf4236da822fe254c10c84e96341
test <test@abc.com>
2016-09-02 12:06:59 +0700

    befor lunch

--- app/src/main/java/rd/angkanasopit/wallop/rdrun/ServiceActivity.java
+++ app/src/main/java/rd/angkanasopit/wallop/rdrun/ServiceActivity.java
@@ -5,6 +5,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -18,6 +19,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class ServiceActivity extends FragmentActivity implements OnMapReadyCallback {
     //Explicit
@@ -29,6 +39,8 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     private double userLatADouble=13.806814, userLngADouble = 100.574725;  //The conecion
     private LocationManager locationManager; //หาพิกัดบนโลก
     private Criteria criteria;
+    private static final String urlPHP = "http://swiftcodingthai.com/rd/edit_location_master.php";
+
 
 
 
@@ -75,6 +87,50 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }   //main method
+
+    private class SynAllUser extends AsyncTask<Void, Void, String> {
+        private Context context;
+        private GoogleMap googleMap;
+        private static final String urlJSON = "http://swiftcodingthai.com/rd/get_user_master.php";
+
+
+
+        public SynAllUser(Context context, GoogleMap googleMap) {
+            this.context = context;
+            this.googleMap = googleMap;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().toString();
+
+
+            } catch (Exception e) {
+                Log.d("2SepV2", "e doIn ==> " + e.toString());
+                return null;
+
+
+            }
+
+
+
+        }   //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("2SepV2", "JSon ==> "+ s);
+        }   //onPost
+
+    }   //SynAllUser Class
+
+
+
 
     @Override
     protected void onResume() {
@@ -165,6 +221,10 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         //to do
         Log.d("1SepV2", "Lat ==> " + userLatADouble);
         Log.d("1SepV2", "Lng ==> " + userLngADouble);
+        editLatLngOnServer();
+
+        createMarker();
+
 
 
         //Post Delay
@@ -177,6 +237,40 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         },1000);
 
     }   //myLoop
+
+    private void createMarker() {
+
+        SynAllUser synAllUser = new SynAllUser(this, mMap);
+        synAllUser.execute();
+
+    }
+
+    private void editLatLngOnServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("id", idString)
+                .add("Lat", Double.toString(userLatADouble))
+                .add("Lng", Double.toString(userLngADouble))
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.d("2SepV1", "e ==> " + e.toString());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.d("2SepV1", "Result ==> " + response.body().string());
+
+            }
+        });
+
+    }   // editLatLng
 
 
 }   //main class
